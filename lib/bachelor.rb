5596dcf51b253b12cec54d6cdf130f4976148899
Samuel Prada <jspradac@gmail.com>
2018-11-14 22:37:55 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -7,7 +7,10 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, players|
+    players.each do |info|
+      if (info.values).include?(occupation)
+        return info["name"]
 end
 
 def count_contestants_by_hometown(data, hometown)
