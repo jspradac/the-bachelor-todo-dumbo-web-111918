ba9360fe7166a0469c63c94d46644b1b01efe381
Samuel Prada <jspradac@gmail.com>
2018-11-14 22:27:44 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,9 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |info|
+    if (info.values).include?("Winner")
+      return (info["name"].split)[0]
+    end 
+  end 
 end
 
 def get_contestant_name(data, occupation)
