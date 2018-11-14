20597829873c26f00b06ff9bda2825fd7ca0759d
Samuel Prada <jspradac@gmail.com>
2018-11-14 23:17:04 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,49 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |info|
+    if (info.values).include?("Winner")
+      return (info["name"].split)[0]
+    end 
+  end 
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, players|
+    players.each do |info|
+      if (info.values).include?(occupation)
+        return info["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
-end
+  counter = 0 
+  data.each do |season, players|
+    players.each do |info|
+      if (info.values).include?(hometown)
+        counter += 1 
+      end
+    end
+  end
+  return counter
+end  
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, players|
+    players.each do |info|
+      if (info.values).include?(hometown)
+        return info["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  a = 0 
+  b = 0 
+  data[season].each do |info|
+    a += info["age"].to_f
+    b += 1 
+  end 
+  return (a/b).round
 end
